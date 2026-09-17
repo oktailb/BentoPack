381097c82280768376582cc745a9825ee0bb84df
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2026-09-17 16:32:55 +0900

    feat (cli): add wrappers for outsiders compatibility layer

---
+++ wrappers/TexturePacker.bat
@@ -0,0 +1,3 @@
+@echo off
+rem Transparent drop-in wrapper for TexturePacker using SpriteStudio CLI
+spritestudio-cli.exe --flavor tp %*
---
+++ wrappers/aseprite.bat
@@ -0,0 +1,3 @@
+@echo off
+rem Transparent batch wrapper for Aseprite using SpriteStudio CLI
+spritestudio-cli.exe --flavor aseprite %*
